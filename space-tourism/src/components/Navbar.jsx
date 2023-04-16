8b5438a73ec1310b800f7b1d52c1ccbe35f434d3
thelouisgram <hadesanoye01@gmail.com>
2023-04-16 13:12:37 +0100

    destination page done

--- space-tourism/src/components/Navbar.jsx
+++ space-tourism/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { logo, hamburger, close } from '../assets/shared'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => { 
+const Navbar = () => {
   const [menu, setMenu] = useState(false)
 
   const toggleMenu = () => {
@@ -10,33 +10,34 @@ const Navbar = () => {
   }
   return (
     // Navbar
-    <nav className='py-6 px-6 flex justify-between items-center ss:p-0 md:py-12  md:pl-16  md:relative'>
+    <nav className='py-6 px-6 flex justify-between items-center ss:pl-10 ss:p-0 md:py-12  md:pl-16  md:'>
       {/* Logo container */}
-      <div className='ss:pl-10 md:pl-0'>
+      <div className=' md:pl-0'>
         <img src={logo} className='h-[40px] ss:h-[50px] w-auto' alt="Logo" />
       </div>
       {/* Navbar Line */}
-      <div className='hidden lg:flex h-[1px] w-[32%] left-44 z-[3] bg-semiWhite absolute'/>
+      <div className='hidden lg:flex h-[1px] w-[32%] left-44 z-[3] bg-semiWhite absolute' />
       {/* > Tablet NavLinks */}
       <div className='glass hidden ss:flex ss:p-10 md:pl-32 md:pr-36 gap-12 md:py-6 tracking-[0.15rem] '>
-          <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>00</h4><h5 className='text-[14px]'>HOME</h5></div></Link>
-          <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>01</h4><h5 className='text-[14px]'>DESTINATION</h5></div></Link>
-          <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>02</h4><h5 className='text-[14px]'>CREW</h5></div></Link>
-          <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>03</h4><h5 className='text-[14px]'>TECHNOLOGY</h5></div></Link>
+        <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>00</h4><h5 className='text-[14px]'>HOME</h5></div></Link>
+        <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>01</h4><h5 className='text-[14px]'>DESTINATION</h5></div></Link>
+        <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>02</h4><h5 className='text-[14px]'>CREW</h5></div></Link>
+        <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>03</h4><h5 className='text-[14px]'>TECHNOLOGY</h5></div></Link>
       </div>
       {/* Menu button */}
       <div onClick={toggleMenu} className='flex ss:hidden cursor-pointer'>
-        <img src={hamburger} className='w-[30px] h-auto '/>
+        <img src={hamburger} className='w-[30px] h-auto ' />
       </div>
       {/* Mobile NavLinks */}
-      {menu && <div className='h-[100vh] w-[250px] flex ss:hidden flex-col items-start gap-6 glass fixed top-0 right-0 p-6'>
-          <div onClick={toggleMenu} className='mb-12 relative flex items-end cursor-pointer w-full'>
+      {menu && <div className='h-[100vh] w-[250px] flex ss:hidden flex-col z-[10]
+      items-start gap-6 glass fixed top-0 right-0 p-6'>
+        <div onClick={toggleMenu} className='mb-12 relative flex items-end cursor-pointer w-full'>
           <img src={close} className='absolute right-0 top-2 w-[30px] h-auto' />
-          </div>
-          <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>00</h4><h5 className='text-[14px]'>HOME</h5></div></Link>
-          <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>01</h4><h5 className='text-[14px]'>DESTINATION</h5></div></Link>
-          <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>02</h4><h5 className='text-[14px]'>CREW</h5></div></Link>
-          <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>03</h4><h5 className='text-[14px]'>TECHNOLOGY</h5></div></Link>
+        </div>
+        <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>00</h4><h5 className='text-[14px]'>HOME</h5></div></Link>
+        <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>01</h4><h5 className='text-[14px]'>DESTINATION</h5></div></Link>
+        <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>02</h4><h5 className='text-[14px]'>CREW</h5></div></Link>
+        <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>03</h4><h5 className='text-[14px]'>TECHNOLOGY</h5></div></Link>
       </div>}
     </nav>
   )
--- space-tourism/src/pages/Destination.jsx
+++ space-tourism/src/pages/Destination.jsx
@@ -1,24 +1,82 @@
-import React, {useState} from 'react'
-import Navbar from '../components/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import data from '../data.json';
 
 const Destination = () => {
-  const [currentSlide, setCurrentSlide] = useState(0)
+	const [ currentSlide, setCurrentSlide ] = useState(0);
+	const destination = data.destinations;
 
+	// Destination Names and paginations
+	const names = destination.map((name, index) => {
+		return (
+			<div
+				onClick={() => {
+					setCurrentSlide(index);
+				}}
+				key={index}
+			>
+				<h4
+					className={`text-babyBlue cursor-pointer font-BarlowCondensed uppercase text-[16px] ss:mt-6 md:mt-0 mb-6 tracking-[0.25rem]
+        ${currentSlide === index ? 'name-active' : 'name'}`}
+				>
+					{name.name}
+				</h4>
+			</div>
+		);
+	});
 
-  return (
-    <section id='destination'>
-        <Navbar />
-        {/* Destination content */}
-          <div className='flex gap-6 tracking-[0.25rem] md:pt-8 md:pl-44'>
-            <h3 className='font-BarlowCondensed text-[24px] font-bold text-semiWhite'>01</h3>
-            <h3 className='font-BarlowCondensed text-[24px] text-white '>PICK YOUR DESTINATION</h3>
-          </div>
-        {/* Left Div */}
-        <div className='flex '>
-          <div></div>
-        </div>
-    </section>
-  )
-}
+	// Rendered Destination Content
+	const content = destination.map((content, index) => {
+		return (
+			<div key={index} className={`${currentSlide === index ? 'active-content' : 'inactive-content'} px-6 ss:px-10 md:px-0 
+      text-center flex-col md:text-left`}>
+				<h1 className="text-white text-[75px] ss:text-[100px] uppercase font-Bellefair">{content.name}</h1>
+				<p className="text-babyBlue font-Barlow text-[14px] ss:text-[20px] md:w-[425px] leading-[24px] ss:leading-[32px]">{content.description}</p>
+				<div className="h-[1px] md:w-[450px] bg-semiWhite my-12" />
+				<div className="flex flex-col gap-6 ss:flex-row md:w-[450px] justify-center ss:gap-16 md:gap-0 md:justify-between ">
+					<div className='ss:text-left'>
+						<h4 className="text-babyBlue font-BarlowCondensed text-[16px] font-bold">AVG. DISTANCE</h4>
+						<h3 className="text-white font-Bellefair text-[28px] md:text-[32px] uppercase">{content.distance}</h3>
+					</div>
+          <div className='ss:text-left'>
+						<h4 className="text-babyBlue font-BarlowCondensed text-[16px] font-bold">EST. TRAVEL TIME</h4>
+            <h3 className="text-white font-Bellefair text-[28px] md:text-[32px] uppercase">{content.travel}</h3>
+					</div>
+				</div>
+			</div>
+		);
+	});
 
-export default Destination
+	const image = destination.map((image, index) => {
+		return (
+      <div key={index} className={`${currentSlide === index ? 'active-content' : 'inactive-content'} max-w-[250px] max-h-[250px] 
+      ss:max-w-[450px] ss:max-h-[450px] md:max-w-[450px] md:max-h-[450px]`}>
+				<img src={image.images.webp} className="w-full h-full" alt="destination image" />
+			</div>
+		);
+	});
+
+	return (
+		// Page content
+		<section id="destination">
+			{/* imported Navbar */}
+			<Navbar />
+			<div className="flex gap-6 tracking-[0.25rem] justify-center mt-8 ss:mt-0 ss:justify-start ss:pt-10 md:pt-8 ss:pl-10 md:pl-44">
+				<h3 className="font-BarlowCondensed text-[18px] ss:text-[24px] font-bold text-semiWhite">01</h3>
+        <h3 className="font-BarlowCondensed text-[18px] ss:text-[24px] text-white ">PICK YOUR DESTINATION</h3>
+			</div>
+			{/* Destination container */}
+			<div className="flex flex-col items-center md:flex-row md:pl-44 md:items-center py-12 ss:py-24 gap-8">
+				{/* Left Div */}
+				<div className="flex flex-1 ">{image}</div>
+				{/* Right Div */}
+				<div className="flex flex-1 flex-col items-center md:items-start">
+					<div className="flex gap-5 ss:gap-8 ">{names}</div>
+					{content}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Destination;
