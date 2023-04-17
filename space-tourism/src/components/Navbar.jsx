1916cdca39274a10c461e786a8dbb14f40a05651
thelouisgram <hadesanoye01@gmail.com>
2023-04-17 19:34:55 +0100

    more bugs

--- space-tourism/src/components/Navbar.jsx
+++ space-tourism/src/components/Navbar.jsx
@@ -15,14 +15,12 @@ const Navbar = () => {
       <div className=' md:pl-0'>
         <img src={logo} className='h-[40px] ss:h-[50px] w-auto' alt="Logo" />
       </div>
-      {/* Navbar Line */}
-      <div className='hidden lg:flex h-[1px] w-[32%] left-44 z-[3] bg-semiWhite absolute' />
       {/* > Tablet NavLinks */}
-      <div className='glass hidden ss:flex ss:p-10 md:pl-32 md:pr-36 gap-12 md:py-6 tracking-[0.15rem] '>
-        <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>00</h4><h5 className='text-[14px]'>HOME</h5></div></Link>
-        <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>01</h4><h5 className='text-[14px]'>DESTINATION</h5></div></Link>
-        <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>02</h4><h5 className='text-[14px]'>CREW</h5></div></Link>
-        <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] hidden md:flex'>03</h4><h5 className='text-[14px]'>TECHNOLOGY</h5></div></Link>
+      <div className='glass hidden ss:flex ss:p-10 md:pl-32 md:pr-36 gap-12 ss:py-7 tracking-[0.15rem] '>
+        <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px] hidden md:flex'>00</h4><h5 className='text-[17px]'>HOME</h5></div></Link>
+        <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px] hidden md:flex'>01</h4><h5 className='text-[17px]'>DESTINATION</h5></div></Link>
+        <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px] hidden md:flex'>02</h4><h5 className='text-[17px]'>CREW</h5></div></Link>
+        <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px] hidden md:flex'>03</h4><h5 className='text-[17px]'>TECHNOLOGY</h5></div></Link>
       </div>
       {/* Menu button */}
       <div onClick={toggleMenu} className='flex ss:hidden cursor-pointer'>
@@ -34,10 +32,10 @@ const Navbar = () => {
         <div onClick={toggleMenu} className='mb-12 relative flex items-end cursor-pointer w-full'>
           <img src={close} className='absolute right-0 top-2 w-[30px] h-auto' />
         </div>
-        <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>00</h4><h5 className='text-[14px]'>HOME</h5></div></Link>
-        <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>01</h4><h5 className='text-[14px]'>DESTINATION</h5></div></Link>
-        <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>02</h4><h5 className='text-[14px]'>CREW</h5></div></Link>
-        <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600]'>03</h4><h5 className='text-[14px]'>TECHNOLOGY</h5></div></Link>
+        <Link to='/'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px]'>00</h4><h5 className='text-[17px]'>HOME</h5></div></Link>
+        <Link to='/destination'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px]'>01</h4><h5 className='text-[17px]'>DESTINATION</h5></div></Link>
+        <Link to='/crew'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px]'>02</h4><h5 className='text-[17px]'>CREW</h5></div></Link>
+        <Link to='/technology'><div className='flex gap-2 text-white font-BarlowCondensed items-center'><h4 className='font-[600] text-[17px]'>03</h4><h5 className='text-[17px]'>TECHNOLOGY</h5></div></Link>
       </div>}
     </nav>
   )
