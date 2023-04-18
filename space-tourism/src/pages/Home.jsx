4a1a8b65b86dd50f5f69cec12b4b9bd9dfb2d1cd
thelouisgram <hadesanoye01@gmail.com>
2023-04-18 15:49:28 +0100

    try again

--- space-tourism/src/pages/Home.jsx
+++ space-tourism/src/pages/Home.jsx
@@ -26,9 +26,9 @@ const Home = () => {
           <Link to='/destination'><div className='bg-white rounded-full h-[200px] w-[200px] 
           md:h-[250px] md:w-[250px] flex items-center justify-center hover:bg-opacity-75
           hover:scale-125 transform transition-all duration-500'>
-            <h2 className='font-Bellefair font-normal text-midnightBlue text-[28px]'>EXPLORE</h2>
-</div>
-</Link>
+            <h3 className='font-Bellefair text-midnightBlue text-[28px]'>EXPLORE</h3>
+        </div>
+        </Link>
         </div>
       </div>
     </section>
